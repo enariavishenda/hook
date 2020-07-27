ab96eb4e35501e2dee5f0f7f48e497c982b9afc6
Enaria Vishenda <matthewfarrel@yandex.ru>
2020-07-28 02:01:58 +0500

    useCallback from function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom";
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect, useCallback} from "react";
 
 const App = () => {
 
-    const [value, setValue] = useState(0)
+    const [value, setValue] = useState(1)
     const [visible, setVisible] = useState(true)
 
     if (visible) {
@@ -30,26 +30,35 @@ const App = () => {
         }
     }
 
+    const getPlanet = (id) => {
+    const _api = 'https://swapi.dev/api/planets/'
+    return fetch(`${_api}${id}`)
+        .then(res => res.json())
+        .then(data => data)
+    }
+
+    const useRequest = (request) => {
+        const [ dataState, setDataState] = useState(null)
+
+        useEffect(() => {
+            let cancel = false
+            request().then(data => !cancel && setDataState(data))
+            return () => cancel = true
+        }, [request])
+        return dataState
+    }
+
+    const usePlanetInfo = (id) => {
+    const request = useCallback( () => getPlanet(id), [ id ] )
+        return useRequest(request)
+    }
+
     const PlanetInfo = ({id}) => {
 
-        const [ label, setLabel] = useState('Noname')
-
-        const _api = 'https://swapi.dev/api/planets/'
-        fetch(`${_api}${id}`)
-            .then(res => res.json())
-            .then(data => console.log(data.name))
-
-
-
-        // useEffect(() => {
-        //     setLabel(() => {
-        //         return {
-        //             label: namePlanet
-        //     }
-        // }, [id])})
+    const data = usePlanetInfo(id)
 
     return (
-        <div>{id} - {label}</div>
+        <div>{id} - { data && data.name}</div>
     )
 }
 ReactDOM.render(<App />,document.getElementById('root'));
